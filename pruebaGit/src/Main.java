ac5a0224df961326bc380c405a62feb85d8072e8
Walid Khalladi Hajji <Walid.khalladi@ikasle.egibide.org>
2024-04-12 21:13:30 +0200

    primera modi main

--- pruebaGit/src/Main.java
+++ pruebaGit/src/Main.java
@@ -2,9 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
-
-
+        System.out.println("hola mundo a todos");
     }
 }
 
